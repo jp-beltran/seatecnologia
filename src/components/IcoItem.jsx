6cc867ed4308ce36350ee38a5b23017ffd3cb163
João Pedro <jpbcs2005@gmail.com>
2024-12-22 03:48:49 -0300

    update2

--- src/components/IcoItem.jsx
+++ src/components/IcoItem.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import ico_items from '../assets/ico_items.svg';
 function IcoItems() {
     return (  
@@ -10,19 +9,8 @@ function IcoItems() {
                 className='w-16 h-16 rounded-3xl mb-2' 
             />
             <h1 className='text-[#4FA1C1] text-sm font-medium'>Item 1</h1>
-=======
-import ico_items from '../assets/iconItem.svg';
-function IcoItems() {
-    return (  
-        <div className='flex flex-col justify-center items-center m-2'>
-            <img src={ico_items} alt="" className='w-16 h-16 [filter:drop-shadow(0px_4px_4px_rgba(0,0,0,0.25))] rounded-3xl 
-            mb-2
-            ' />
-            <h1>TEXTO</h1>
->>>>>>> 8d5098449d70f51ac586ba3b13ac1196dcb161d3
+
+
         </div>
-            
     );
 }
-
-export default IcoItems;
--- src/components/Items.jsx
+++ src/components/Items.jsx
@@ -15,17 +15,10 @@ const arrIcoItems =[
 
 function Items() {
     return (  
-<<<<<<< HEAD
         <div className="flex justify-start items-center p-6  w-[1370px] h-[156px] bg-white rounded-[20px] mb-8 relative ">
                 <div className=" flex flex-row justify-start items-center w-full h-full z-10 absolute " >
                     {arrIcoItems.map((item, index) => (
                         <div key={index} className="flex flex-col items-center ml-12">
-=======
-        <div className="flex justify-start items-center p-6  w-full h-[156px] bg-white rounded-[20px] mb-8 relative">
-                <div className=" flex flex-row justify-start items-center w-full h-full z-10 absolute ml-17" >
-                    {arrIcoItems.map((item, index) => (
-                        <div key={index} className="flex items-center ml-12">
->>>>>>> 8d5098449d70f51ac586ba3b13ac1196dcb161d3
                             <item.icon />
                             <h2 className=" text-[color:var(--Dark,#272F33)] text-center  text-md font-medium">Concluído</h2>
                         </div>
@@ -33,13 +26,8 @@ function Items() {
                     
                 </div>
 
-<<<<<<< HEAD
                 <div className="flex flex-row justify-start items-top w-full h-full z-0 absolute -translate-y-6">
                     <img src={LineItems} alt="" className=" ml-20 w-[1027px] "/>
-=======
-                <div className="flex flex-row justify-start items-top w-full h-full z-0 absolute">
-                    <img src={LineItems} alt="" className="ml-31 w-[1090px]"/>
->>>>>>> 8d5098449d70f51ac586ba3b13ac1196dcb161d3
                 </div>
 
  
